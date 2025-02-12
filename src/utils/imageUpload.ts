e6b793e01504649fcb16fb3aa4640d4bfa517e77
DonggunLim <Ldonggun6766@gmail.com>
2025-02-12 14:11:12 +0900

    feat: S3 image upload 함수 추가

---
+++ src/utils/imageUpload.ts
@@ -0,0 +1,28 @@
+import { getPresignedUrl } from "@/apis/image.api";
+import axios from "axios";
+
+export const imageUpload = async (file: File) => {
+  try {
+    const presignedUrl = await getPresignedUrl({
+      fileName: `${file.name}-${Date.now()}`,
+      fileType: file.type,
+    });
+    await axios.put(presignedUrl, file, {
+      headers: { "Content-Type": file.type },
+    });
+    console.log(presignedUrl);
+    return presignedUrl.split("?")[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const multipleImageUpload = async (files: File[]) => {
+  try {
+    const promises = files.map((file) => imageUpload(file));
+    const response = await Promise.all(promises);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
