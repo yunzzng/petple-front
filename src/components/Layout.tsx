1937a38b1e65043d68c508cf599e0e31039c9637
yunzzng <leejiyun0114@naver.com>
2025-02-13 20:29:26 +0900

    높이 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import '../index.css';
+import Header from './Header';
 
 const BaseLayout = () => {
   return (
     <>
       <div className="wrapper">
+        <Header />
         <div className="content">
           <Outlet />
         </div>
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -14,9 +14,8 @@ const Map: FC<MapProps> = ({ locations = [] }) => {
   useEffect(() => {
     if (!isSuccess || !window.kakao || !window.kakao.maps || !container.current) return;
 
-    // 지도 기본 설정
-    const position = new window.kakao.maps.LatLng(33.450701, 126.570667);
-    const options = { center: position, level: 4 };
+    const defaultPosition = new window.kakao.maps.LatLng(33.450701, 126.570667);
+    const options = { center: defaultPosition, level: 12 };
 
     const map = new window.kakao.maps.Map(container.current, options);
 
@@ -24,17 +23,27 @@ const Map: FC<MapProps> = ({ locations = [] }) => {
     const imageSize = new window.kakao.maps.Size(24, 35);
     const markerImage = new window.kakao.maps.MarkerImage(markerImg, imageSize);
 
-    // 마커 추가
-    locations?.forEach((loc) => {
-      const markerPosition = new window.kakao.maps.LatLng(loc.lat, loc.lng);
+    // 기본 마커
+    if (locations.length === 0) {
       const marker = new window.kakao.maps.Marker({
-        position: markerPosition,
+        position: defaultPosition,
         image: markerImage,
       });
-
       marker.setMap(map);
-    });
+    }
 
+    // 사용자 선택
+    if (locations.length > 0) {
+      locations.forEach((location) => {
+        const markerPosition = new window.kakao.maps.LatLng(location.lat, location.lng);
+        const marker = new window.kakao.maps.Marker({
+          position: markerPosition,
+          image: markerImage,
+        });
+
+        marker.setMap(map);
+      });
+    }
   }, [isSuccess, locations]);
 
   return <div id="map" className={styles.mapContainer} ref={container}></div>;
