f67617490a62bbc186e029188ec1bc78e21b7c25
DonggunLim <Ldonggun6766@gmail.com>
2025-02-17 16:36:17 +0900

    feat: Accordion 공통 컴포넌트 추가

---
+++ src/components/Accordion/components/Content.tsx
@@ -0,0 +1,10 @@
+import { FC, PropsWithChildren, useMemo } from "react";
+import { useAccordionContext } from "./Root";
+import { AccordionContentProps } from "../types";
+
+const Content: FC<AccordionContentProps> = ({ children, className }) => {
+  const { isOpen } = useAccordionContext();
+  return isOpen && <div className={className}>{children}</div>;
+};
+
+export default Content;
---
+++ src/components/Accordion/index.ts
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import Content from "./components/Content";
+import Root from "./components/Root";
+import Title from "./components/Title";
+
+const Accordion = {
+  Root,
+  Button,
+  Content,
+  Title,
+};
+
+export default Accordion;
