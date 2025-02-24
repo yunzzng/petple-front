93dc4ebc1f1fa70af5518db0314cdc6369c4eaac
DonggunLim <Ldonggun6766@gmail.com>
2025-02-24 23:38:08 +0900

    feat: ChatList Component 추가

---
+++ src/pages/Chat/components/ChatList.tsx
@@ -0,0 +1,27 @@
+import { ChatMessageType } from "@/types/chat.type";
+import styles from "./chatList.module.css";
+import { AuthStore } from "@/types/user.type";
+import ChatMessage from "./ChatMessage";
+
+interface ChatListProps {
+  messages: ChatMessageType[];
+  user: AuthStore;
+}
+
+const ChatList = ({ messages, user }: ChatListProps) => {
+  return (
+    <main className={styles.wrapper}>
+      <ul>
+        {messages.map((message, index) => (
+          <ChatMessage
+            message={message}
+            nickname={user.userNickName}
+            key={index}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default ChatList;
