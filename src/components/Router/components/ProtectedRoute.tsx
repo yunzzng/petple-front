a68f6a2a77bbc4e9d421aac68f8e99d5d181b98e
DonggunLim <Ldonggun6766@gmail.com>
2025-02-26 01:30:31 +0900

    feat: ProtectedRoute component 추가

---
+++ src/components/Router/components/ProtectedRoute.tsx
@@ -0,0 +1,11 @@
+import userAuthStore from "@/zustand/userAuth";
+import { FC, PropsWithChildren } from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
+  const { userId } = userAuthStore();
+
+  return !!userId ? children : <Navigate to="/login" replace={true} />;
+};
+
+export default ProtectedRoute;
