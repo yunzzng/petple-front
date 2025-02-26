e17520b6a92cd0859d69f29dbe13159a4799be83
baekahyeon <4641090@naver.com>
2025-02-26 23:40:57 +0900

    룰렛 속도 조절, 모달로 랜덤이름 보여주기

--- src/pages/Roulette/index.tsx
+++ src/pages/Roulette/index.tsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import style from "./roulette.module.css";
-import { Modal } from "@/components";
+import { Button, Modal } from "@/components";
+import { getRandomName } from "./roulleteName";
+import { RoulleteName } from "./roulleteName";
 
 const Roulette = () => {
-  const [color, setColor] = useState<string>("");
+  const [color, setColor] = useState<keyof typeof RoulleteName>("black");
   const [rotation, setRotation] = useState(0);
   const [spinning, setSpinning] = useState(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [randomName, setRandomName] = useState<string>("");
 
-  const startRoulette = () => {
+  const startRoulette = (color: keyof typeof RoulleteName) => {
     if (spinning) return; // 이미 돌아가고있으면 return
-
+    const name = getRandomName(color);
+    if (name) {
+      setRandomName(name);
+    }
     setSpinning(true);
 
     let angle = rotation; // 현재 각도에서 시작
-    let speed = 10; // 초기 속도 (클수록 빠름)
-    let deceleration = 0.99; // 감속 비율 (1에 가까울수록 더 오래 회전)
+    let speed = 20; // 초기 속도 (클수록 빠름)
+    let deceleration = 0.98; // 감속 비율 (1에 가까울수록 더 오래 회전)
     let minSpeed = 0.2; // 최소 속도 (너무 빨리 멈추지 않도록 설정)
-    let finalAngle = 360 * 15 + Math.floor(Math.random() * 360); // 10바퀴 + 랜덤 각도
 
     const spin = () => {
       if (speed <= minSpeed) {
@@ -50,39 +55,62 @@ const Roulette = () => {
   return (
     <div className={style.roulette_total_wrap}>
       <div className={style.roulette_content}>
-        <h1 className={style.h1}>펫네임 랜덤룰렛 🎡</h1>
-        <div>
-          <div>
-            <p>나의 반려동물 색상은 ?</p>
+        <h1 className={style.h1}>🫧 펫네임 랜덤룰렛 🫧</h1>
+        <div className={style.color_wrap}>
+          <div className={style.colorPick}>
+            <p>나의 반려동물 색상은 👉</p>
             <p>{color}</p>
           </div>
-          <div>
-            <button onClick={startRoulette} disabled={spinning}>
-              START!
-            </button>
+          <div className={style.button_wrap}>
+            <button
+              className={style.black}
+              onClick={() => setColor("black")}
+            ></button>
+            <button
+              className={style.white}
+              onClick={() => setColor("white")}
+            ></button>
+            <button
+              className={style.ivory}
+              onClick={() => setColor("ivory")}
+            ></button>
+            <button
+              className={style.gold}
+              onClick={() => setColor("gold")}
+            ></button>
+            <button
+              className={style.brown}
+              onClick={() => setColor("brown")}
+            ></button>
+            <button
+              className={style.gray}
+              onClick={() => setColor("gray")}
+            ></button>
+            <button
+              className={style.triColor}
+              onClick={() => setColor("triColor")}
+            ></button>
           </div>
         </div>
-        <div className={style.button_wrap}>
-          <button className={style.black}></button>
-          <button className={style.white}></button>
-          <button className={style.ivory}></button>
-          <button className={style.gold}></button>
-          <button className={style.brown}></button>
-          <button className={style.gray}></button>
-          <button className={style.colorBtn}></button>
+        <div className={style.startBtn}>
+          <Button onClick={() => startRoulette(color)} disabled={spinning}>
+            START!
+          </Button>
         </div>
-        <div className={style.roulette_container}>
-          {/* <div className={style.pin}></div> */}
-          <div
-            className={style.roulette}
-            style={{ transform: `rotate(${rotation}deg)` }}
-          >
-            <div className={style.line_1}></div>
-            <div className={style.line_2}></div>
-            <div className={style.line_3}></div>
-            <div className={style.line_4}></div>
-            <div className={style.line_5}></div>
-            <div className={style.line_6}></div>
+        <div className={style.roulette_wrap}>
+          <div className={style.roulette_container}>
+            <div className={style.pin}></div>
+            <div
+              className={style.roulette}
+              style={{ transform: `rotate(${rotation}deg)` }}
+            >
+              <div className={style.line_1}></div>
+              <div className={style.line_2}></div>
+              <div className={style.line_3}></div>
+              <div className={style.line_4}></div>
+              <div className={style.line_5}></div>
+              <div className={style.line_6}></div>
+            </div>
           </div>
         </div>
         <Modal.Root
@@ -92,7 +120,13 @@ const Roulette = () => {
           className={style.modal}
         >
           <Modal.Backdrop className={style.backdrop} />
-          <Modal.Content className={style.content}>반려동물 이름</Modal.Content>
+          <Modal.Content className={style.content}>
+            <Modal.Close className={style.close}>
+              <p>x</p>
+            </Modal.Close>
+            <p> 펫플이 추천하는 반려동물 이름은 🐾 </p>
+            <p className={style.name}>{randomName}</p>
+          </Modal.Content>
         </Modal.Root>
       </div>
     </div>
