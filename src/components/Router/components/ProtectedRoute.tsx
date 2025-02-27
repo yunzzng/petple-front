f0d39ccdb4cb33bf8e00d4e91965a83574174f51
DonggunLim <Ldonggun6766@gmail.com>
2025-02-27 17:30:44 +0900

    fix: ProtectedRoute user정보 참조 문제 loader로 해결

--- src/components/Router/components/ProtectedRoute.tsx
+++ src/components/Router/components/ProtectedRoute.tsx
@@ -1,25 +1,9 @@
-import Loading from "@/components/Loading";
 import userAuthStore from "@/zustand/userAuth";
-import { FC, PropsWithChildren, useEffect, useState } from "react";
+import { FC, PropsWithChildren } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
-  const [isLoading, setIsLoding] = useState(true);
   const { userId } = userAuthStore();
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setIsLoding(false);
-    }, 500);
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [userId]);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return !!userId ? children : <Navigate to="/login" replace={true} />;
 };
 
