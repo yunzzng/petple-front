73f6b63e28dd22d3b5130340ad73383f069fa0e9
DonggunLim <Ldonggun6766@gmail.com>
2025-02-27 11:45:50 +0900

    fix: protectedRoute userId 참조 할수있게 변경

--- src/components/Router/components/ProtectedRoute.tsx
+++ src/components/Router/components/ProtectedRoute.tsx
@@ -1,9 +1,24 @@
+import Loading from "@/components/Loading";
 import userAuthStore from "@/zustand/userAuth";
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
+  const [isLoading, setIsLoding] = useState(true);
   const { userId } = userAuthStore();
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setIsLoding(false);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [userId]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return !!userId ? children : <Navigate to="/login" replace={true} />;
 };
