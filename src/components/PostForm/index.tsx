402553dfee675afdadfb3954ac411c4c821a798e
yunzzng <leejiyun0114@naver.com>
2025-02-27 10:56:35 +0900

    경로 수정

--- src/components/PostForm/index.tsx
+++ src/components/PostForm/index.tsx
@@ -1,160 +1,262 @@
-import styles from "./postform.module.css";
-import ChipInput from "@/components/ChipInput";
-import { ChangeEvent, useRef, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
-// import uploadIcon from "/images/icons/upload.svg";
+import { useForm } from "react-hook-form";
+import style from "./petForm.module.css";
+import Button from "../Button";
+import userAuthStore from "@/zustand/userAuth";
+import { ChangeEvent, FC, useRef, useState } from "react";
+import { imageUpload } from "@/utils/imageUpload";
+import { Pet } from "@/types/user.type";
+import { deletePet, updatePetInfo } from "@/apis/profile.api";
+import { petSchema } from "@/consts/zodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import FormHeader from "./components/FormHeader";
-import { PostFormData, PostFormFields } from "@/types/post.type";
-import { postFormSchema } from "@/consts/zodSchema";
 
-interface PostFormProps {
-  post?: PostFormData;
-  requestType: "create" | "update";
-  onSubmit: (data: PostFormFields) => void;
-  onClickDelete?: () => void;
+interface PetInfoProps {
+  name?: string;
+  age?: string;
+  breed?: string;
+  _id?: string;
+  image?: string;
 }
 
-const PostForm = ({
-  requestType,
-  post,
-  onSubmit,
-  onClickDelete,
-}: PostFormProps) => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [previewImages, setPreviewImages] = useState<string[]>(
-    post?.images ?? []
-  );
-  const {
-    control,
-    register,
-    watch,
-    handleSubmit,
-    setError,
-    setValue,
-    formState: { errors, isDirty },
-  } = useForm<PostFormFields>({
-    defaultValues: {
-      tags: post?.tags ?? [],
-      images: post?.images ?? [],
-      description: post?.description ?? "",
-    },
-    resolver: zodResolver(postFormSchema),
-    mode: "onSubmit",
-  });
+const PetForm: FC<PetInfoProps> = (props) => {
+  const { name, age, _id, image, breed } = props;
 
-  const handleChangeFileInput = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    const files = Array.from(e.target.files);
-    const currentImages = watch("images") || [];
-    const totalImages: Array<File | string> = [...currentImages, ...files];
-    if (totalImages.length > 10) {
-      setError("images", { message: "최대 10장까지 등록할 수 있습니다." });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<Pet>({
+    defaultValues: {
+      name: name || "",
+      age: age || "",
+      breed: breed || "",
+      _id: _id || "",
+      image: image || "",
+    },
+    resolver: zodResolver(petSchema),
+    mode: "onBlur",
+  });
+  const { userId, setUserPet, userPet } = userAuthStore();
+  const [previewImg, setPreviewImg] = useState<string>(image || "");
+  const [file, setFile] = useState<File | null>(null);
+  const [edit, setEdit] = useState<boolean>(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  // useEffect(() => {
+  //   setValue("name", name || "");
+  //   setValue("age", age || "");
+  //   setValue("breed", breed || "");
+  //   setValue("_id", _id || "");
+  //   setValue("image", image || "");
+  // }, [_id, name, age, breed, image]);
+
+  const handleClickFile = () => {
+    fileInputRef?.current?.click();
+  };
+
+  const handleChangeImg = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const selectedFile = e.target.files[0];
+      setFile(selectedFile);
+      const previewUrl = URL.createObjectURL(selectedFile);
+      setPreviewImg(previewUrl);
+    }
+  };
+
+  const onSubmit = async (petData: Pet) => {
+    if (
+      name === petData.name &&
+      age === petData.age &&
+      image === previewImg &&
+      breed === petData.breed
+    ) {
+      setEdit(false);
       return;
     }
 
-    setValue("images", totalImages);
-    const filterdFileImages = totalImages
-      .filter((image) => image instanceof File)
-      .map((file) => URL.createObjectURL(file));
-    setPreviewImages((prev) => [...prev, ...filterdFileImages]);
+    let imageUrl = previewImg;
+
+    try {
+      if (file) {
+        imageUrl = await imageUpload(file);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    const updatedPet = await updatePetInfo(userId!, petData, _id!, imageUrl);
+
+    if (updatedPet) {
+      const updatePetList = userPet?.map((pet) =>
+        pet._id === updatedPet._id ? updatedPet : pet
+      );
+
+      if (updatePetList) {
+        userAuthStore.setState({
+          userPet: updatePetList,
+        });
+
+        alert("반려동물 프로필이 업데이트되었습니다.");
+        setEdit(false);
+      }
+    }
   };
 
-  const handleClickDeleteImage = (index: number) => {
-    const currentImages = watch("images") || [];
-    const updatedPreviews = previewImages.filter((_, i) => i !== index);
-    const updatedImages = currentImages.filter((_, i) => i !== index);
-    setPreviewImages(updatedPreviews);
-    setValue("images", updatedImages);
+  const deletePetInfo = async () => {
+    try {
+      const deletedPet = await deletePet(userId!, _id!);
+
+      if (deletedPet) {
+        alert("반려동물 프로필이 삭제되었습니다.");
+
+        const updateList = userPet?.filter((pet) => pet._id !== _id);
+        if (updateList) {
+          setUserPet(updateList);
+          return;
+        }
+      }
+    } catch (error) {
+      console.error("반려동물 정보 삭제 실패", error);
+    }
   };
 
-  const handleResizeHeight = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    e.target.style.height = "auto";
-    e.target.style.height = `${e.target.scrollHeight}px`;
+  const handleClose = () => {
+    setEdit(false);
+    reset({
+      name: name || "",
+      age: age || "",
+      breed: breed || "",
+      _id: _id || "",
+      image: image || "",
+    });
+    setPreviewImg(image || "");
   };
 
   return (
-    <>
-      <FormHeader
-        onClickSubmit={handleSubmit((data) => {
-          if (!isDirty) return;
-          onSubmit(data);
-        })}
-        onClickDelete={onClickDelete}
-        reqeustType={requestType}
-      />
-      <form className={styles.form}>
-        <Controller
-          name="tags"
-          control={control}
-          defaultValue={[]}
-          render={({ field }) => (
-            <>
-              <ChipInput {...field} values={post?.tags} />
-              {errors.tags && (
-                <p className={styles.error}>{errors.tags.message}</p>
-              )}
-            </>
-          )}
-        />
-        <input
-          type="file"
-          accept="image/*"
-          multiple
-          {...register("images", {
-            validate: (files) =>
-              files.length <= 10 ? true : "최대 10장까지 등록할 수 있습니다.",
-          })}
-          ref={(e) => {
-            register("images").ref(e);
-            fileInputRef.current = e;
-          }}
-          onChange={handleChangeFileInput}
-          className={styles.hiddenFileInput}
-        />
-        <ul className={styles.fileUpload_container}>
-          <li
-            className={styles.uploaded_file}
-            onClick={() => fileInputRef.current?.click()}
-          >
-            <img
-              src={"/images/icons/upload.svg"}
-              alt="업로드 버튼"
-              className={styles.default_image}
-            />
-          </li>
-          {previewImages.map((image, index) => (
-            <li
-              className={styles.uploaded_file}
-              key={`preview-image-${index}`}
-              onClick={() => handleClickDeleteImage(index)}
-            >
-              <img
-                src={image}
-                alt="업로드 버튼"
-                className={styles.previewImage}
-              />
-            </li>
-          ))}
-        </ul>
-        <p className={styles.info}>* 최대 10장까지 등록 가능합니다.</p>
-        {errors.images && (
-          <p className={styles.error}>{errors.images.message}</p>
-        )}
-        <textarea
-          id="description"
-          maxLength={5000}
-          placeholder="내용을 입력해주세요"
-          className={styles.description}
-          onInput={handleResizeHeight}
-          {...register("description")}
-        ></textarea>
-        {errors.description && (
-          <p className={styles.error}>{errors.description.message}</p>
+    <div className={style.petForm_total_wrap}>
+      <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
+        {edit ? (
+          <div className={style.form_box}>
+            <div className={style.button_wrap}>
+              <Button type="submit" className={style.button}>
+                저장
+              </Button>
+              <Button className={style.button} onClick={handleClose}>
+                닫기
+              </Button>
+            </div>
+            <ul className={style.edit_pet_ul}>
+              <div className={style.edit_pet_ul_div}>
+                <li className={style.list}>
+                  <img
+                    onClick={handleClickFile}
+                    src={previewImg}
+                    className={style.img}
+                  />
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleChangeImg}
+                    className={style.file}
+                  />
+                </li>
+                <li className={style.list}>
+                  <div className={style.list_div}>
+                    <label>이름:</label>
+                    <input
+                      className={style.input}
+                      placeholder="이름"
+                      {...register("name", { required: true })}
+                    />
+                  </div>
+                  <div>
+                    {errors.name && (
+                      <p className={style.errors}>{errors.name.message}</p>
+                    )}
+                  </div>
+                </li>
+                <li className={style.list}>
+                  <div className={style.list_div}>
+                    <label>나이:</label>
+                    <input
+                      className={style.input}
+                      placeholder="나이"
+                      {...register("age", { required: true })}
+                    />
+                  </div>
+                  <div>
+                    {errors.age && (
+                      <p className={style.errors}>{errors.age.message}</p>
+                    )}
+                  </div>
+                </li>
+                <li>
+                  <div className={style.list_div}>
+                    <label>품종:</label>
+                    <input
+                      className={style.input}
+                      placeholder="품종"
+                      {...register("breed", { required: true })}
+                    />
+                  </div>
+                  <div>
+                    {errors.breed && (
+                      <p className={style.errors}>{errors.breed.message}</p>
+                    )}
+                  </div>
+                </li>
+              </div>
+
+              {/* <li>
+              <label>성별</label>
+              <input {...register("gender", { required: true })} />
+              <div>
+                <Button type="button" onClick={() => setValue("gender", "남")}>
+                  남아
+                </Button>
+                <Button type="button" onClick={() => setValue("gender", "여")}>
+                  여아
+                </Button>
+              </div>
+            </li> */}
+            </ul>
+          </div>
+        ) : (
+          <div className={style.form_box}>
+            <div className={style.button_wrap}>
+              <Button
+                className={style.button}
+                type="button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setEdit(true);
+                }}
+              >
+                수정
+              </Button>
+              <Button
+                className={style.button}
+                type="button"
+                onClick={deletePetInfo}
+              >
+                삭제
+              </Button>
+            </div>
+            <ul className={style.pet_ul}>
+              <li>
+                <img src={image} className={style.img} />
+              </li>
+              <li>
+                <p>
+                  {name} / {age} / {breed}
+                </p>
+              </li>
+            </ul>
+          </div>
         )}
       </form>
-    </>
+    </div>
   );
 };
 
-export default PostForm;
+export default PetForm;
