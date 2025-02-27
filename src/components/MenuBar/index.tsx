7227552e8d45fd1825171faa8bc3fede50b96904
yunzzng <leejiyun0114@naver.com>
2025-02-27 11:25:10 +0900

    포스트폼 수정

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -1,33 +1,33 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./menubar.module.css";
-import home from "/images/home.png";
-import chat from "/images/chat.png";
-import community from "/images/community.png";
-import menu from "/images/menu.png";
-import walking from "/images/walking.png";
+// import home from "/images/home.png";
+// import chat from "/images/chat.png";
+// import community from "/images/community.png";
+// import menu from "/images/menu.png";
+// import walking from "/images/walking.png";
 
 const MenuBar = () => {
   const navigate = useNavigate();
   return (
     <footer className={styles.footer}>
       <div className={styles.menu_wrap} onClick={() => navigate("/")}>
-        <img src={home} className={styles.img} />
+        <img src={"/images/home.png"} className={styles.img} />
         <a>홈</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/petfriends")}>
-        <img src={chat} className={styles.img} />
+        <img src={"/images/chat.png"} className={styles.img} />
         <a>펫프렌즈</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/community")}>
-        <img src={community} className={styles.img} />
+        <img src={"/images/community.png"} className={styles.img} />
         <a>커뮤니티</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/petwalk")}>
-        <img src={walking} className={styles.img} />
+        <img src={"/images/walking.png"} className={styles.img} />
         <a>펫산책</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/menu")}>
-        <img src={menu} className={styles.img} />
+        <img src={"/images/menu.png"} className={styles.img} />
         <a>메뉴</a>
       </div>
     </footer>
