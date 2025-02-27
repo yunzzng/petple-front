8c59ac6973af644132e298991b87611ad7d59aab
baekahyeon <4641090@naver.com>
2025-02-27 13:24:25 +0900

    alt 추가

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -11,23 +11,23 @@ const MenuBar = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.menu_wrap} onClick={() => navigate("/")}>
-        <img src={home} className={styles.img} />
+        <img src={home} className={styles.img} alt="메인홈 바로가기" />
         <a>홈</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/petfriends")}>
-        <img src={chat} className={styles.img} />
+        <img src={chat} className={styles.img} alt="펫프렌즈 바로가기" />
         <a>펫프렌즈</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/community")}>
-        <img src={community} className={styles.img} />
+        <img src={community} className={styles.img} alt="커뮤니티 바로가기" />
         <a>커뮤니티</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/petwalk")}>
-        <img src={walking} className={styles.img} />
+        <img src={walking} className={styles.img} alt="펫산책 바로가기" />
         <a>펫산책</a>
       </div>
       <div className={styles.menu_wrap} onClick={() => navigate("/menu")}>
-        <img src={menu} className={styles.img} />
+        <img src={menu} className={styles.img} alt="메뉴 바로가기" />
         <a>메뉴</a>
       </div>
     </footer>
