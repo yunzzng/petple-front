e94d4b9111c700d923b0a8b5e9015cdbf6c7e588
baekahyeon <4641090@naver.com>
2025-02-28 16:02:41 +0900

    수정없음

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -10,14 +10,24 @@ interface PaginationProps {
   setPage: (page: number) => void;
 }
 
-const Pagination = ({ page, totalPages, startPage, endPage, setPage }: PaginationProps) => {
+const Pagination = ({
+  page,
+  totalPages,
+  startPage,
+  endPage,
+  setPage,
+}: PaginationProps) => {
   const pageNumbers = useMemo(() => {
     return [...Array(endPage - startPage + 1)].map((_, i) => startPage + i);
   }, [startPage, endPage]);
 
   return (
     <div className={styles.pagination}>
-      <Button onClick={() => setPage(Math.max(page - 1, 1))} disabled={page === 1} className={styles.pageButton}>
+      <Button
+        onClick={() => setPage(Math.max(page - 1, 1))}
+        disabled={page === 1}
+        className={styles.pageButton}
+      >
         이전
       </Button>
 
@@ -25,13 +35,19 @@ const Pagination = ({ page, totalPages, startPage, endPage, setPage }: Paginatio
         <Button
           key={pageNumber}
           onClick={() => setPage(pageNumber)}
-          className={`${styles.pageNumber} ${page === pageNumber ? styles.activePage : ""}`}
+          className={`${styles.pageNumber} ${
+            page === pageNumber ? styles.activePage : ""
+          }`}
         >
           {pageNumber}
         </Button>
       ))}
 
-      <Button onClick={() => setPage(Math.min(page + 1, totalPages))} disabled={page === totalPages} className={styles.pageButton}>
+      <Button
+        onClick={() => setPage(Math.min(page + 1, totalPages))}
+        disabled={page === totalPages}
+        className={styles.pageButton}
+      >
         다음
       </Button>
     </div>
