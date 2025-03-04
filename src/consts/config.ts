c1ad1859d73f10a904a185a4e59a1a2dfa43ee0e
DonggunLim <Ldonggun6766@gmail.com>
2025-03-05 02:16:52 +0900

    fix: backendUrl 수정

--- src/consts/config.ts
+++ src/consts/config.ts
@@ -2,7 +2,7 @@ export const config = {
   app: {
     backendUrl:
       import.meta.env.MODE === "development"
-        ? "https://localhost:3000"
-        : "https://kdt-react-2-team01.elicecoding.com",
+        ? "https://localhost:3001"
+        : "https://kdt-react-2-team01.elicecoding.com/api",
   },
 };
