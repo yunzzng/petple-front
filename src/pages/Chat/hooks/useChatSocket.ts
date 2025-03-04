2b9ec96e151362717dbf7063f584dc8c85cbb827
DonggunLim <Ldonggun6766@gmail.com>
2025-03-05 03:20:37 +0900

    fix: socket 통신 테스트

--- src/pages/Chat/hooks/useChatSocket.ts
+++ src/pages/Chat/hooks/useChatSocket.ts
@@ -51,7 +51,7 @@ const useChatSocket = ({
 
   useEffect(() => scrollToBottom(), [messages]);
   useEffect(() => {
-    const socket = io(`${config.app.backendUrl}/chat`, {
+    const socket = io(`${config.app.backendUrl}`, {
       transports: ["websocket"],
       reconnectionDelayMax: 10000,
     });
