460363380f60f0524f57a904f77d046a70b6642c
baekahyeon <4641090@naver.com>
2025-03-04 16:17:16 +0900

    avartar 수정

--- src/components/UI/Avartar/index.tsx
+++ src/components/UI/Avartar/index.tsx
@@ -1,21 +1,43 @@
+import userAuthStore from "@/zustand/userAuth";
 import style from "./avartar.module.css";
 import { FC } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 interface AvartarProps {
   onClick?: () => void;
-  image?: string | null;
   className?: string | null;
+  image?: string;
   alt?: string;
+  creator?: {
+    _id: string;
+    name: string;
+    email: string;
+    nickName: string;
+    profileImage: string;
+  };
 }
 
 const Avartar: FC<AvartarProps> = (props) => {
-  const { onClick, image, className, alt } = props;
+  const { onClick, className, alt, creator, image } = props;
+  const { userNickName } = userAuthStore();
+  const { nickname } = useParams<string>();
+  const navigate = useNavigate();
+  const profileImage = creator?.profileImage;
+
+  const handleAvatarClick = async () => {
+    if (!creator) return;
+    if (userNickName === creator.nickName || nickname) {
+      return navigate("/profile");
+    }
+
+    return navigate(`/profile/${creator.nickName}`);
+  };
 
   return (
     <>
-      <a onClick={onClick} className={style.a}>
+      <a onClick={onClick || handleAvatarClick} className={style.a}>
         <img
-          src={image || "/images/profile.png"}
+          src={profileImage || image || "/images/profile.png"}
           className={className || style.image}
           alt={alt || "프로필 이미지"}
         />
